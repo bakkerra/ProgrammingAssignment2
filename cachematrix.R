02383a529e5b8c7ea7a1130e2e3a59ef1deb2cf8
bakkerra <rachaelbakker2013@u.northwestern.edu>
2018-07-13 12:52:00 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+##This system of functions stores a matrix and its inverse in an environment
+##in order to access them quickly
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix makes a list of functions that gets and stores the 
+##inverse of a matrix object,x
 
+makeCacheMatrix<-function(x=matrix()){
+  ##set inverse matrix object S to zero
+  S<-NULL
+  ##define set function that stores input matrix in parent environment and resets inverse to zero
+  set<-function(y){
+    x<<-y
+    S<<-NULL
+  }
+  ##define get function in order to access input matrix
+  get<-function() x
+  ##define setsolve to store inverse matrix in parent environment
+  setsolve<-function(solvedmatrix) S<<-solvedmatrix
+  ##define getsolve to access inverse matrix
+  getsolve<-function() S
+  list(set= set, get=get, setsolve=setsolve, getsolve=getsolve)
 }
 
 
-## Write a short comment describing this function
+##cacheSolve finds the inverse of the matrix stored in the makeCachematrix environment. 
+##It either gets it from the cache or calculates and stores it
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<- function(x,...){
+  ##access inverse matrix stored in makecachematrix environment
+  S<-x$getsolve()
+  ##if it is not null, access inverse matrix from the cache and print inverse matrix
+  if(!is.null(S)){
+    message("getting cached data")
+    return(S)
+  }
+  ##access input matrix stored in makecachematrix environment
+  data<-x$get()
+  ##make and store the inverse matrix in makecachematrix environment, print inverse matrix
+  S<-solve(data,...)
+  x$setsolve(S)
+  S
 }
